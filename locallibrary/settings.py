c1f44dbe7072e046dea66000a245a5fefe3e494e
Ron <ron.newman@gmail.com>
2022-01-05 17:29:24 -0700

    Added three Whitenoise settings as an attempt to resolve 'Missing staticfiles manifest entry' error on Heroku.

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -153,6 +153,11 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# Attempting to resolve 'Missing staticfiles manifest entry' error on Heroku:
+WHITENOISE_USE_FINDERS = True
+WHITENOISE_MANIFEST_STRICT = False
+WHITENOISE_ALLOW_ALL_ORIGINS = True
+
 
 # Activate Django-Heroku 
 django_heroku.settings(locals())
